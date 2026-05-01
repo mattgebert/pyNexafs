01f7b29ef809b3127ed2c1678019d63407dcf539
Matt Gebert <mattheuu.gebert@gmail.com>
2026-05-02 00:30:41 +1000

    refactor(parsers/__init__.py): add types and docstrings to parser module items

--- pyNexafs/parsers/__init__.py
+++ pyNexafs/parsers/__init__.py
@@ -6,31 +6,34 @@ in GUI applications.
 """
 
 # Base objects
-from pyNexafs.parsers._base import parserBase, parserMeta
+from pyNexafs.parsers.base import parserBase, parserMeta
 
 # Specific Parsers
 from pyNexafs.parsers import au
 from pyNexafs.parsers.au import SXR_NEXAFS, MEX1_NEXAFS, MEX2_NEXAFS
 
 # Define loaders with string representation.
-parser_loaders = {
+parser_registry: dict[str, type[parserBase]] = {
     "au SXR:NEXAFS": au.SXR_NEXAFS,
     "au MEX1:NEXAFS": au.MEX1_NEXAFS,
     "au MEX2:NEXAFS": au.MEX2_NEXAFS,
 }
+"""A dictionary of parser loaders, which are used to register parsers in GUI applications."""
 
 # Check that all parsers are subclasses of the base parser.
-for parser in parser_loaders.values():
+for parser in parser_registry.values():
     assert issubclass(parser, parserBase), f"{parser} is not a subclass of parserBase."
 
 # Check that no parser names overlap. Important for GUIs.
-parser_names = [parser_name for parser_name in parser_loaders.keys()]
+parser_names: list[str] = list(parser_registry.keys())
+"""A list of parser names available in the `parser_registry` dictionary."""
+
 assert len(parser_names) == len(set(parser_names)), "Parser names overlap."
 
 __all__ = [
     "parserBase",
     "parserMeta",
-    "parser_loaders",
+    "parser_registry",
     "au",
     "SXR_NEXAFS",
     "MEX1_NEXAFS",
